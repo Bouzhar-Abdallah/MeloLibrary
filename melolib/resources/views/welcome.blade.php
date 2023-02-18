7ecbdf5311cd89fa289e02b7e0a4d777c8ab1135
abdallah <abddoullah.bouzhar@gmail.com>
2023-02-18 17:57:31 +0100

    save

--- melolib/resources/views/welcome.blade.php
+++ melolib/resources/views/welcome.blade.php
@@ -17,11 +17,11 @@
 </head>
 
 <body class="antialiased">
-    <nav class="px-2 bg-white border-gray-200 dark:bg-gray-900 dark:border-gray-700">
+    <nav class="px-2 bg-white border-gray-200 dark:bg-saffron dark:border-gray-700">
         <div class="container flex flex-wrap items-center justify-between mx-auto">
             <a href="#" class="flex items-center">
-                <img src="https://flowbite.com/docs/images/logo.svg" class="h-6 mr-3 sm:h-10" alt="Flowbite Logo" />
-                <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+                <img src="{{ asset('images/svg/logo-no-background.svg') }}" class="h-10 mr-3 sm:h-10" alt="Flowbite Logo" />
+                
             </a>
             <button data-collapse-toggle="navbar-dropdown" type="button" class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-dropdown" aria-expanded="false">
                 <span class="sr-only">Open main menu</span>
--- melolib/tailwind.config.js
+++ melolib/tailwind.config.js
@@ -11,6 +11,24 @@ module.exports = {
 
     theme: {
         extend: {
+            colors: {
+                transparent: 'transparent',
+                current: 'currentColor',
+                'cadet': '#2B2D42',
+                'cadeth': '#404363',
+                'cadethh': '#DADBE7',
+                'cadethhh': '#F3F3F7',
+                'P_navy': '#424874',
+                'powder': '#FDFFFC',
+                'mauve': '#361B2B',
+                'saffron': '#F8E16C',
+                'wsaffron': '#FFF15C',
+                'ryb': '#FF1B1C',
+                'cultured': '#F7F7F9',
+                'mediumaqua': '#41E2BA',
+                'goldenrod': '#F3F9D2',
+                'middlegreen': '#558564',
+                },
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
