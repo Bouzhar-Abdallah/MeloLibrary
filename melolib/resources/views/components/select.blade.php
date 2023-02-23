d208a6a7bb5778468481e1c214d7a398cdef5926
abdallah <abddoullah.bouzhar@gmail.com>
2023-02-23 11:09:28 +0100

    song form creation finalised

--- melolib/resources/views/components/select.blade.php
+++ melolib/resources/views/components/select.blade.php
@@ -1,8 +1,11 @@
-<span class="mt-4">
-<label class="" for="{{ $name }}[]" class="sr-only"> {{ $name}}s </label></span>
-<select multiple id="{{ $name }}" name="{{ $name }}[]" class="block px-0 w-full text-sm text-gray-500 bg-transparent border-0 border-b-2 border-gray-200 appearance-none dark:text-gray-400 dark:border-gray-700 focus:outline-none focus:ring-0 focus:border-gray-200 peer">
+<div class="">
+<label class="" for="{{ $name }}[]" class="sr-only"> {{ $label}}s 
+    <span class="text-xs font-thin text-gray-400">selection multiple</span>
+</label>
+<select multiple id="{{ $name }}" name="{{ $name }}[]" class="bg-white border block px-2 w-full text-sm text-gray-500 bg-transparent appearance-none dark:text-gray-400 dark:border-gray-700 focus:outline-none focus:ring-0 focus:border-gray-200 peer">
     
     @foreach ($options as $value)
     <option value="{{ $value->id }}">{{ $value->name }}</option>
     @endforeach
 </select>
+</div>
--- melolib/resources/views/song/new.blade.php
+++ melolib/resources/views/song/new.blade.php
@@ -2,11 +2,11 @@
 {{ $songs }}
 </pre> -->
 <x-app-layout>
-  <div class="p-4 mx-auto">
+  <div class=" mx-auto">
     <form method="post" action=" {{ route('admin.song.save') }} " class="space-y-8 divide-y divide-gray-200" enctype="multipart/form-data">
-    @csrf
+      @csrf
       <div class=" grid grid-cols-2 ">
-        <div class="p-8">
+        <div class="p-4">
 
 
           <div class="space-y-6 sm:space-y-5">
@@ -24,11 +24,16 @@
               <label for="lyrics" class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"> lyrics </label>
               <div class="mt-1 sm:mt-0 sm:col-span-2">
                 <textarea id="lyrics" name="lyrics" rows="3" class="max-w-lg shadow-sm block w-full focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm border border-gray-300 rounded-md"></textarea>
-                <p class="mt-2 text-sm text-gray-500">Write a few sentences lyrics yourself.</p>
+                <p class="mt-2 text-sm text-gray-500">write lyrics of the song :</p>
               </div>
             </div>
 
-
+            <div class="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
+              <label for="release_date" class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"> release date</label>
+              <div class="mt-1 sm:mt-0 sm:col-span-2">
+                <input type="date" name="release_date" id="release_date" autocomplete="family-name" class="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-md">
+              </div>
+            </div>
 
             <div class="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
               <label for="cover" class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"> Cover photo </label>
@@ -52,13 +57,17 @@
             </div>
 
             <div class="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
-              <label for="clip" class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"> clip : </label>
+              <label for="clip" class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"> Audio : </label>
               <div class="mt-1 sm:mt-0 sm:col-span-2">
                 <div class="max-w-lg flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
                   <div class="space-y-1 text-center">
-                    <svg class="mx-auto h-12 w-[40px] text-black" stroke="currentColor" fill="none" viewBox="0 0 48 48" aria-hidden="true">
+                    <!-- <svg class="mx-auto h-12 w-[40px] text-black" stroke="currentColor" fill="none" viewBox="0 0 48 48" aria-hidden="true">
                       <path d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                    </svg> -->
+                    <svg class="mx-auto h-12 w-[40px] text-black" stroke="currentColor" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1" stroke="currentColor" class="w-6 h-6">
+                      <path stroke-linecap="round" stroke-linejoin="round" d="M9 9l10.5-3m0 6.553v3.75a2.25 2.25 0 01-1.632 2.163l-1.32.377a1.803 1.803 0 11-.99-3.467l2.31-.66a2.25 2.25 0 001.632-2.163zm0 0V2.25L9 5.25v10.303m0 0v3.75a2.25 2.25 0 01-1.632 2.163l-1.32.377a1.803 1.803 0 01-.99-3.467l2.31-.66A2.25 2.25 0 009 15.553z" />
                     </svg>
+
                     <div class="flex text-sm text-gray-600">
                       <label for="clip" class="relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500">
                         <span>Upload a file</span>
@@ -66,47 +75,26 @@
                       </label>
                       <p class="pl-1">or drag and drop</p>
                     </div>
-                    <p class="text-xs text-gray-500">PNG, JPG, GIF up to 10MB</p>
+                    <p class="text-xs text-gray-500">MP3</p>
                   </div>
                 </div>
               </div>
             </div>
+            
           </div>
         </div>
 
-        <div class="p-8 ">
+        <div class="p-4 ">
 
           <div class="space-y-6 sm:space-y-5">
-            <div class="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:pt-5">
-              <label for="url" class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"> url </label>
-              <div class="mt-1 sm:mt-0 sm:col-span-2">
-                <input type="text" name="url" id="url" autocomplete="given-name" class="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-md">
-              </div>
-            </div>
-
-
-
-            <div class="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
-              <label for="duration" class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"> duration</label>
-              <div class="mt-1 sm:mt-0 sm:col-span-2">
-                <input type="number" name="duration" id="duration" autocomplete="family-name" class="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-md">
-              </div>
-            </div>
-
-            <div class="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
-              <label for="release_date" class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"> release date</label>
-              <div class="mt-1 sm:mt-0 sm:col-span-2">
-                <input type="date" name="release_date" id="release_date" autocomplete="family-name" class="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-md">
-              </div>
-            </div>
 
             <x-select name="artist" label="Artist" />
             <x-select name="band" label="Band" />
             <x-select name="genre" label="Genre" />
             <x-select name="writer" label="writer" />
-            <x-select name="language" label="language" />
+            <x-select name="language" label="Language" />
 
-            
+
 
 
           </div>
